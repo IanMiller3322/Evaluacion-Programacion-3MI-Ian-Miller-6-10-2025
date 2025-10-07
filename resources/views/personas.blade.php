8c07628f46369b0c0c54945f45d40557c4ac0f5d
IanMiller3322 <ianmiller3322@gmail.com>
2025-10-06 22:15:23 -0300

    Cuarto commit con primer version del listado de personas

---
+++ resources/views/personas.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Listado de Usuarios</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="mb-4">Listado de Personas</h1>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Telefono</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</body>
+</html>
