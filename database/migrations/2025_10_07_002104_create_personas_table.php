902ff8e43ab399e66da4c812b9205a76a9e210d1
IanMiller3322 <ianmiller3322@gmail.com>
2025-10-06 22:14:16 -0300

    Tercer commit con primer version del controller para personas

--- database/migrations/2025_10_07_002104_create_personas_table.php
+++ database/migrations/2025_10_07_002104_create_personas_table.php
@@ -14,7 +14,7 @@ class CreatePersonasTable extends Migration
     public function up()
     {
         Schema::create('personas', function (Blueprint $table) {
-            $table->id();
+            $table->id("id");
             $table->string("nombre", 20);
             $table->string("apellido", 20);
             $table->string("telefono", 9);
